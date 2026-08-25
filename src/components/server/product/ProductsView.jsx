d10d8fe66b0d1f3f88195556c167a992625f27b6
Aneesh Mathew <aneeshmatthew@gmail.com>
2026-08-25 16:21:30 -0700

    fix

--- src/components/server/product/ProductsView.jsx
+++ src/components/server/product/ProductsView.jsx
@@ -6,7 +6,7 @@ export default function ProductsView({ categories, category }) {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
       <div className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-900 mb-1">
+        <h1 className="text-3xl font-bold mb-1">
           {category ? (
             <span className="capitalize">{category}</span>
           ) : (
